8469abefb3f17cd459ae2751cd77d2b96bd759e5
Augustino Nguyen <trongphu.cbl49@gmail.com>
2020-06-28 04:57:18 +0700

    Add templates and Pagination frontend OK - Thanks Huan

--- WebSearchCourses/WebSearchCourses/urls.py
+++ WebSearchCourses/WebSearchCourses/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from search import views as search_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('search/', search_views.search),
+    path('search2/', search_views.search2),
 ]
--- WebSearchCourses/search/views.py
+++ WebSearchCourses/search/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from .documents import CourseDocument
+from course.models import Course
 # Create your views here.
+
+# def search(request):
+#     q = request.GET.get('q')
+#     if q:
+#         courses = CourseDocument.search().query('match', name=q)
+#     else:
+#         courses = ''
+
+#     return render(request, 'search/search.html', {'courses': courses})
+
+def search(request):
+    courses = Course.objects.all()
+    
+    data = {'courses': courses, 'size': len(courses)}
+    return render(request, 'search/courses.html', data)
+
+def search2(request):
+    return render(request, 'search/courses2.html')
